6acb2b265e21e0eacdfa127f6b5fcbf0b61fdc71
Humphrey Burchell <humphreyburchell@gmail.com>
2024-12-22 19:44:29 +0000

    getIfSingleFile

--- GTResources/AWFYBenchmarksLookUp.java
+++ GTResources/AWFYBenchmarksLookUp.java
@@ -7,6 +7,31 @@ import java.nio.file.Paths;
 import java.io.IOException;
 
 public class AWFYBenchmarksLookUp {
+
+    public static boolean getIfSingleFile(String benchmarkName) {
+        try {
+            String filePath = "/home/hb478/repos/GTSlowdownSchedular/GTResources/AWFY_Benchmarks.json";
+            // Read the JSON file content as a String
+            String jsonString = new String(Files.readAllBytes(Paths.get(filePath)));
+
+            // Parse the JSON string into a JSONObject
+            JSONObject jsonObject = new JSONObject(jsonString);
+
+            // Retrieve "extra_args" for the specified benchmark name
+            if (jsonObject.getJSONObject("AWFY Benchmarks").has(benchmarkName)) {
+                return jsonObject.getJSONObject("AWFY Benchmarks")
+                        .getJSONObject(benchmarkName).getBoolean("SingleFile");
+            } else {
+                System.out.println("Benchmark not found: " + benchmarkName);
+                return false; // Return a default/error value if the benchmark is not found
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false; // Return a default/error value if file reading fails
+        }
+    }
+
     // Method to retrieve "extra_args" based on the benchmark name
     public static int getExtraArgs(String benchmarkName) {
         try {
